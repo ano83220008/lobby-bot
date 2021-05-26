8ac963a0496338962581cb510afe6e032982cf63
ano83220008 <84181827+ano83220008@users.noreply.github.com>
2021-05-26 21:45:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
+from discord import Embed
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 cate_lobby_name = os.environ['MATCHING_CATEGORY']
 
