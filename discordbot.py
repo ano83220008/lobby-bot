4e7978551b5f7ec70235115b10dc21f3638762b4
ano83220008 <84181827+ano83220008@users.noreply.github.com>
2021-05-26 21:38:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,49 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 cate_lobby_name = os.environ['MATCHING_CATEGORY']
 
+class LobbyManager:
+    
+    guild=None
+    def __init__(self, guild):
+        self.guild=guild
+
+    async def close_lobby(self, name):
+        lobby = find_lobby_without_status(self.guild, name)
+        if(lobby!=None):
+            await lobby.delete()
+
+    async def create_lobby(self, name):
+        cate = get_category(self.guild, cate_lobby_name)
+        lobby = await cate.create_text_channel("ready-" + name)
+        return lobby
+
+    async def set_status(self, name, status):
+        cate = get_category(self.guild, cate_lobby_name)
+        for channel in cate.channels:
+            if(channel.name.endswith(name)):
+                i = channel.name.find('-')
+                await channel.edit(name = status + '-' + channel.name[i+1:])
+
+    def exists_lobby(self, name):
+        return find_lobby_without_status(self.guild, name)!=None
+
+def is_my_lobby(channel, member):
+    return without_status(channel.name)==member.name
+
+def without_status(name):
+    i = name.find('-')
+    return name[i+1:]
+
+def get_category(guild, key):
+    if(type(key) is int):
+        for cat in guild.categories:
+            if cat.id==key:
+                return cat
+    if(type(key) is str):
+        for cat in guild.categories:
+            if cat.name==key:
+                return cat
+
 #ロビーを名前のみで検索
 def find_lobby_without_status(guild, name):
     cate = get_category(guild, cate_lobby_name)
